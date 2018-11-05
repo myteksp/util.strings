3e583252dc4edc329d356511a80153ea64a15bcc
dmitry <dmitry@192.168.193.104>
2018-11-05 15:42:54 +0200

    mmmmmm

--- src/main/java/com/gf/util/string/encryption/Keys.java
+++ src/main/java/com/gf/util/string/encryption/Keys.java
@@ -1,94 +1,58 @@
 package com.gf.util.string.encryption;
 
 import java.nio.ByteBuffer;
-import java.security.KeyFactory;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.gf.util.string.JSON;
 
 public final class Keys {
 
-	private static final byte[] toLongArray(final long l1, final long l2) {
-		final ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES + Long.BYTES);
-		buffer.putLong(l1);
-		buffer.putLong(l2);
-		return buffer.array();
-	}
-
 	private static final byte[] randomSeed() {
-		if (Math.random() > 0.5)
-			return toLongArray(System.currentTimeMillis(), System.nanoTime());
-		else
-			return toLongArray(System.nanoTime(), System.currentTimeMillis());
+		final long l1 = System.currentTimeMillis();
+		final long l2 = System.nanoTime();
+		final double l3 = Math.random();
+		final double dist = Math.random();
+		final ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES + Long.BYTES + Double.BYTES + Double.BYTES);
+		if (dist > 0.7) {
+			buffer.putLong(l1);
+			buffer.putLong(l2);
+			buffer.putDouble(l3);
+		}else if(dist > 0.4) {
+			buffer.putLong(l2);
+			buffer.putDouble(l3);
+			buffer.putLong(l1);
+		}else {
+			buffer.putDouble(l3);
+			buffer.putLong(l1);
+			buffer.putLong(l2);
+		}
+		buffer.putDouble(dist);
+		return buffer.array();
 	}
 
 	public static final KeyPair createKeyPair() {
 		try {
-			return createKeyPair(2048, Algorithm.RSA);
+			return createKeyPair(3072, Algorithm.RSA);
+		} catch (final NoSuchAlgorithmException e) {
+			return null;
+		}
+	}
+	
+	public static final KeyPair createKeyPair(final int len) {
+		try {
+			return createKeyPair(len, Algorithm.RSA);
 		} catch (final NoSuchAlgorithmException e) {
 			return null;
 		}
 	}
 
-	private static final Map<String, String> toString(final PrivateKey privateKey) {
-		if (privateKey == null)
-			return new HashMap<String, String>(0);
-		final Map<String, String> res = new HashMap<String, String>(3);
-		res.put("algorithm", privateKey.getAlgorithm());
-		res.put("format", privateKey.getFormat());
-		res.put("key", Base64.getUrlEncoder().encodeToString(privateKey.getEncoded()));
-		return res;
-	}
-
-	private static final Map<String, String> toString(final PublicKey publicKey) {
-		if (publicKey == null)
-			return new HashMap<String, String>(0);
-		final Map<String, String> res = new HashMap<String, String>(3);
-		res.put("algorithm", publicKey.getAlgorithm());
-		res.put("format", publicKey.getFormat());
-		res.put("key", Base64.getUrlEncoder().encodeToString(publicKey.getEncoded()));
-		return res;
-	}
-
-	private static final PrivateKey toPrivate(final Map<String, String> map) throws Throwable {
-		return KeyFactory.getInstance(map.get("algorithm")).generatePrivate(new PKCS8EncodedKeySpec(Base64.getUrlDecoder().decode(map.get("key"))));
-	}
-
-	private static final PublicKey toPublic(final Map<String, String> map) throws Throwable {
-		return KeyFactory.getInstance(map.get("algorithm")).generatePublic(new X509EncodedKeySpec(Base64.getUrlDecoder().decode(map.get("key"))));
-	}
-
 	public static final KeyPair fromJson(final String json){
 		try {
-			final _keyPairObj obj = JSON.fromJson(json, _keyPairObj.class);
-			return new KeyPair() {
-				private final PrivateKey privateKey = toPrivate(obj.get("private"));
-				private final PublicKey publicKey = toPublic(obj.get("public"));
-				@Override
-				public final PublicKey getPublic() {
-					return publicKey;
-				}
-				@Override
-				public final PrivateKey getPrivate() {
-					return privateKey;
-				}
-				@Override
-				public final String toString() {
-					final Map<String, Map<String, String>> map = new HashMap<String, Map<String, String>>(3);
-					map.put("private", Keys.toString(privateKey));
-					map.put("public", Keys.toString(publicKey));
-					return JSON.toJson(map);
-				}
-			};
+			return JSON.fromJson(json, KeyPair.class);
 		}catch(final Throwable t) {
 			throw new RuntimeException(t);
 		}
@@ -98,37 +62,120 @@ public final class Keys {
 		final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(algorithm.toString());
 		keyGen.initialize(len, new SecureRandom(randomSeed()));
 		final java.security.KeyPair pair = keyGen.generateKeyPair();
-		return new KeyPair() {
-			private final PrivateKey privateKey = pair.getPrivate();
-			private final PublicKey publicKey = pair.getPublic();
-			@Override
-			public final PrivateKey getPrivate() {
-				return privateKey;
-			}
-			@Override
-			public final PublicKey getPublic() {
-				return publicKey;
-			}
-			@Override
-			public final String toString() {
-				final Map<String, Map<String, String>> map = new HashMap<String, Map<String, String>>(3);
-				map.put("private", Keys.toString(privateKey));
-				map.put("public", Keys.toString(publicKey));
-				return JSON.toJson(map);
-			}
-		};
+		final PrivateKey privateKey = pair.getPrivate();
+		final PublicKey publicKey = pair.getPublic();
+		
+		final KeyPair res = new KeyPair();
+		final Key prvt = res.privateKey = new Key();
+		final Key pblc = res.publicKey = new Key();
+		prvt.type = KeyType.PRIVATE;
+		pblc.type = KeyType.PUBLIC;
+		pblc.algorithm = prvt.algorithm = algorithm;
+		prvt.format = privateKey.getFormat();
+		pblc.format = publicKey.getFormat();
+		prvt.key = Base64.getUrlEncoder().encodeToString(privateKey.getEncoded());
+		pblc.key = Base64.getUrlEncoder().encodeToString(publicKey.getEncoded());
+		return res;
 	}
 
 	public static enum Algorithm{
 		DSA, RSA, EC, DH
 	}
 
-	public static interface KeyPair {
-		PrivateKey getPrivate();
-		PublicKey getPublic();
+	public static final class KeyPair {
+		public Key privateKey;
+		public Key publicKey;
+		@Override
+		public final int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((privateKey == null) ? 0 : privateKey.hashCode());
+			result = prime * result + ((publicKey == null) ? 0 : publicKey.hashCode());
+			return result;
+		}
+		@Override
+		public final boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final KeyPair other = (KeyPair) obj;
+			if (privateKey == null) {
+				if (other.privateKey != null)
+					return false;
+			} else if (!privateKey.equals(other.privateKey))
+				return false;
+			if (publicKey == null) {
+				if (other.publicKey != null)
+					return false;
+			} else if (!publicKey.equals(other.publicKey))
+				return false;
+			return true;
+		}
+		@Override
+		public final String toString() {
+			return "KeyPair [privateKey=" + privateKey + ", publicKey=" + publicKey + "]";
+		}
+		public final String toJson() {
+			return JSON.toJson(this);
+		}
+		public final String toPrettyJson() {
+			return JSON.toPrettyJson(this);
+		}
 	}
-
-	public static final class _keyPairObj extends HashMap<String, Map<String, String>> implements Map<String, Map<String, String>>{
-		private static final long serialVersionUID = -5183653179350382169L;
+	
+	public static final class Key{
+		public KeyType type;
+		public Algorithm algorithm;
+		public String format;
+		public String key;
+		@Override
+		public final int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((algorithm == null) ? 0 : algorithm.hashCode());
+			result = prime * result + ((format == null) ? 0 : format.hashCode());
+			result = prime * result + ((key == null) ? 0 : key.hashCode());
+			result = prime * result + ((type == null) ? 0 : type.hashCode());
+			return result;
+		}
+		@Override
+		public final boolean equals(final Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Key other = (Key) obj;
+			if (algorithm == null) {
+				if (other.algorithm != null)
+					return false;
+			} else if (!algorithm.equals(other.algorithm))
+				return false;
+			if (format == null) {
+				if (other.format != null)
+					return false;
+			} else if (!format.equals(other.format))
+				return false;
+			if (key == null) {
+				if (other.key != null)
+					return false;
+			} else if (!key.equals(other.key))
+				return false;
+			if (type != other.type)
+				return false;
+			return true;
+		}
+		@Override
+		public final String toString() {
+			return "Key [type=" + type + ", algorithm=" + algorithm + ", format=" + format + ", key=" + key + "]";
+		}
+	}
+	
+	public static enum KeyType{
+		PRIVATE, PUBLIC
 	}
 }
--- src/test/java/com/gf/util/string/KeysTest.java
+++ src/test/java/com/gf/util/string/KeysTest.java
@@ -12,7 +12,7 @@ public class KeysTest {
 		final int len = 5;
 		System.out.println("Warming up");
 		for (int i = 0; i < len; i++) {
-			Keys.createKeyPair().toString();
+			Keys.createKeyPair().toJson();
 		}
 		System.out.println("Warming up - DONE.");
 		final long time = System.currentTimeMillis();
@@ -21,23 +21,23 @@ public class KeysTest {
 		}
 		System.out.println("Time for " + len + " keys:" + (System.currentTimeMillis() - time));
 		for (int i = 0; i < 10; i++) {
-			System.out.println(Keys.createKeyPair().toString());
+			System.out.println(Keys.createKeyPair().toJson());
 		}
 	}
 	
 	@Test
 	public final void algorithmsKeyTest() throws NoSuchAlgorithmException{
-		System.out.println("DH" + Keys.createKeyPair(512, Keys.Algorithm.DH).toString());
-		System.out.println("DSA" + Keys.createKeyPair(1024, Keys.Algorithm.DSA).toString());
-		System.out.println("EC" + Keys.createKeyPair(128, Keys.Algorithm.EC).toString());
-		System.out.println("RSA" + Keys.createKeyPair(1024, Keys.Algorithm.RSA).toString());
+		System.out.println("DH" + Keys.createKeyPair(512, Keys.Algorithm.DH).toJson());
+		System.out.println("DSA" + Keys.createKeyPair(1024, Keys.Algorithm.DSA).toJson());
+		System.out.println("EC" + Keys.createKeyPair(128, Keys.Algorithm.EC).toJson());
+		System.out.println("RSA" + Keys.createKeyPair(1024, Keys.Algorithm.RSA).toJson());
 	}
 	
 	@Test
 	public final void serializationTest() throws NoSuchAlgorithmException{
-		System.out.println("Parsed - DH" + Keys.fromJson(Keys.createKeyPair(512, Keys.Algorithm.DH).toString()).toString());
-		System.out.println("Parsed - DSA" + Keys.fromJson(Keys.createKeyPair(1024, Keys.Algorithm.DSA).toString()).toString());
-		System.out.println("Parsed - EC" + Keys.fromJson(Keys.createKeyPair(128, Keys.Algorithm.EC).toString()).toString());
-		System.out.println("Parsed - RSA" + Keys.fromJson(Keys.createKeyPair(1024, Keys.Algorithm.RSA).toString()).toString());
+		System.out.println("Parsed - DH" + Keys.fromJson(Keys.createKeyPair(512, Keys.Algorithm.DH).toJson()).toJson());
+		System.out.println("Parsed - DSA" + Keys.fromJson(Keys.createKeyPair(1024, Keys.Algorithm.DSA).toJson()).toJson());
+		System.out.println("Parsed - EC" + Keys.fromJson(Keys.createKeyPair(128, Keys.Algorithm.EC).toJson()).toJson());
+		System.out.println("Parsed - RSA" + Keys.fromJson(Keys.createKeyPair(1024, Keys.Algorithm.RSA).toJson()).toJson());
 	}
 }
