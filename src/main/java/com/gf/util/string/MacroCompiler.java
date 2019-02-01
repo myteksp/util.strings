b84aae0abd94c129708ee98cb4ab3a81e504f8d0
dmitry <dmitry@192.168.193.23>
2019-02-01 14:55:34 +0200

    object to str

--- src/main/java/com/gf/util/string/MacroCompiler.java
+++ src/main/java/com/gf/util/string/MacroCompiler.java
@@ -17,7 +17,7 @@ public final class MacroCompiler {
 			return script;
 		if (serializer == null)
 			throw new NullPointerException("serializer was null");
-		
+
 		final int paramInitialLen = 15;
 		final Map<String, String> paramsStr = new HashMap<String, String>(params.size() + 4);
 		int paramLen = 0;
@@ -26,7 +26,7 @@ public final class MacroCompiler {
 			paramLen += v.length();
 			paramsStr.put(e.getKey(), v);
 		}
-		
+
 
 		final StringBuilder sb = new StringBuilder((int) ((script.length() + paramLen) * 1.5));
 
@@ -106,12 +106,12 @@ public final class MacroCompiler {
 			return script;
 		if (params.isEmpty())
 			return script;
-		
+
 		final int paramInitialLen = 15;
 		int paramLen = 0;
 		for(final Entry<String, String> e : params.entrySet()) 
 			paramLen += e.getValue().length();
-		
+
 		final StringBuilder sb = new StringBuilder((int) ((script.length() + paramLen) * 1.5));
 
 		CharsIterator.iterate(script, new CharsIterator.CharConsumer() {
@@ -194,9 +194,9 @@ public final class MacroCompiler {
 		int paramLen = 0;
 		for(final String e : params) 
 			paramLen += e.length();
-		
+
 		final StringBuilder sb = new StringBuilder((int) ((script.length() + paramLen) * 1.5));
-		
+
 		CharsIterator.iterate(script, new CharsIterator.CharConsumer() {
 			private MacroState state = MacroState.EXPECTING_START;
 			private StringBuilder param = new StringBuilder(paramInitialLen);
@@ -262,14 +262,74 @@ public final class MacroCompiler {
 				}
 			}
 		});
-		
+
 		return sb.toString();
 	}
-	
+
+	private static final void stackElementToStr(final StackTraceElement e, final StringBuilder sb) {
+		if (e.isNativeMethod()) 
+			sb.append("[NATIVE]->");
+		sb.append("[File(").append(e.getFileName()).append(")]->")
+		.append("[Class(").append(e.getClassName()).append(")]->")
+		.append("[Line(").append(e.getLineNumber()).append(")]");
+	}
+
+	private static final String stackTraceToStr(final Throwable t) {
+		final StackTraceElement[] trace = t.getStackTrace();
+		final int len = trace.length;
+		final int last = len - 1;
+		final StringBuilder sb = new StringBuilder((len * 60) + 30);
+		sb.append("(");
+		for (int i = 0; i < last; i++) {
+			stackElementToStr(trace[i], sb);
+			sb.append(", ");
+		}
+		stackElementToStr(trace[last], sb);
+		sb.append(")");
+		return sb.toString();
+	}
+
+	private static String toStr(final Throwable t) {
+		final String message = t.getMessage();
+		final String loaclMessage = t.getLocalizedMessage();
+		if (message.equalsIgnoreCase(loaclMessage)) {
+			return message + " : " + stackTraceToStr(t);
+		}else {
+			return message + "(" + loaclMessage + ") : " + stackTraceToStr(t);
+		}
+	}
+
 	private static final String objStr(final Object o) {
 		if (o == null)
 			return "[null]";
-		return o.toString();
+		
+		final Class<?> clz = o.getClass();
+		
+		if (clz.isPrimitive())
+			return o.toString();
+
+		if (o instanceof Throwable) 
+			return toStr((Throwable)o);
+		
+		if (clz.isArray()) {
+			final Object[] arr = (Object[])o;
+			final int len = arr.length;
+			final int last = len - 1;
+			final StringBuilder sb = new StringBuilder(len * 30);
+			sb.append("[");
+			for (int i = 0; i < last; i++) {
+				sb.append(objStr(arr[i])).append(", ");
+			}
+			sb.append(objStr(arr[last]));
+			sb.append("]");
+			return sb.toString();
+		}
+		
+		try {
+			return JSON.toJson(o);
+		}catch(final Throwable t) {
+			return o.toString();
+		}
 	}
 	private static final String[] cvrt(final Object[] o) {
 		final int len = o.length;
@@ -278,7 +338,7 @@ public final class MacroCompiler {
 			res[i] = objStr(o[i]);
 		return res;
 	}
-	
+
 	public static final String compileObjects(final String script, 
 			final Object[] raw){
 		if (script == null)
@@ -287,16 +347,16 @@ public final class MacroCompiler {
 			return script;
 		if (raw.length == 0)
 			return script;
-		
+
 		final String[] params = cvrt(raw);
-		
+
 		final int paramInitialLen = 4;
 		int paramLen = 0;
 		for(final String e : params) 
 			paramLen += e.length();
-		
+
 		final StringBuilder sb = new StringBuilder((int) ((script.length() + paramLen) * 1.5));
-		
+
 		CharsIterator.iterate(script, new CharsIterator.CharConsumer() {
 			private MacroState state = MacroState.EXPECTING_START;
 			private StringBuilder param = new StringBuilder(paramInitialLen);
@@ -362,10 +422,10 @@ public final class MacroCompiler {
 				}
 			}
 		});
-		
+
 		return sb.toString();
 	}
-	
+
 	public static final String compileInline(final String script, 
 			final String ...params){
 		return compile(script, params);
@@ -384,9 +444,9 @@ public final class MacroCompiler {
 		int paramLen = 0;
 		for(final String e : params) 
 			paramLen += e.length();
-		
+
 		final StringBuilder sb = new StringBuilder((int) ((script.length() + paramLen) * 1.5));
-		
+
 		CharsIterator.iterate(script, new CharsIterator.CharConsumer() {
 			private MacroState state = MacroState.EXPECTING_START;
 			private StringBuilder param = new StringBuilder(paramInitialLen);
@@ -452,7 +512,7 @@ public final class MacroCompiler {
 				}
 			}
 		});
-		
+
 		return sb.toString();
 	}
 
